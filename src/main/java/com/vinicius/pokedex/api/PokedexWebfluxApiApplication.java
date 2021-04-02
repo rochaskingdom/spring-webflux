498f86df16b25a6266114b1471455514e84207e9
Vinicius Ridrigues Rocha <viniciusr.rocha@outlook.com>
2021-04-01 22:27:17 -0300

    Projeto iniciado

---
+++ src/main/java/com/vinicius/pokedex/api/PokedexWebfluxApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.pokedex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexWebfluxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexWebfluxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinicius/pokedex/api/PokedexWebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.pokedex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexWebfluxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
